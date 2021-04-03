9e73fa60361479fcaf2c857c03faf3bb50fdbca6
juliagarlor <julia.garcia.lorente@gmail.com>
2021-04-03 20:34:28 +0200

    deployed to heroku

--- Frontend/pokemon/src/app/services/trainer.service.ts
+++ Frontend/pokemon/src/app/services/trainer.service.ts
@@ -1,13 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment'
 
 @Injectable({
   providedIn: 'root'
 })
 export class TrainerService {
 
-  url = 'http://localhost:8080'
+  url = environment.baseUrl;
 
   constructor(
     private httpClient: HttpClient
